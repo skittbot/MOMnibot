428831c17598b2aa5221a473fb2ac2edbcaa3be7
Jolene <sulfur.mermaid@gmail.com>
2019-02-20 13:31:03 -0500

    error handler updated, can now be toggled on/off

--- commands/warntest.js
+++ commands/warntest.js
@@ -1,3 +1,11 @@
 exports.run = (client, message) => {
   client.emit("warn","oopsie poopsies");
+  try{
+    var o = null;
+    console.log(o.length);
+  }
+  catch(err) {
+    console.log(err);
+    console.log("oh lol maybe it does work")
+  }
 };
--- events/error.js
+++ events/error.js
@@ -1,10 +1,11 @@
-module.exports = (client, e) => {
+module.exports = async (client, e) => {
   console.error(e);
-  client.fetchUser(client.config.ownerID)
-  .then(result => result.createDM())
-  .then(newResult => newResult.send(e))
-  .catch(wasError => {
-      return console.log("Failed to send message: " + wasError);
-  });
-
+  if (client.config.senderror) {
+    client.fetchUser(client.config.ownerID)
+      .then(result => result.createDM())
+      .then(newResult => newResult.send(e))
+      .catch(wasError => {
+        return console.log("Failed to send message: " + wasError);
+    });
+  }
 };
