908df7eed40f97642d29fa90ffb4255ea31ff049
Jolene <sulfur.mermaid@gmail.com>
2019-04-12 23:04:09 -0400

    ughhhh i messed up lol

---
+++ config-example.js
@@ -0,0 +1,16 @@
+const config = {
+  // bot token
+  "token": "bot token here",
+  // my ID
+  "ownerID": "owner ID here",
+  // other server settings
+  "settings": {
+    "debug": "false",
+    "prefix": "~",
+    "artboardName":"art-gallery",
+    "pronouns":false
+  }
+
+};
+
+module.exports = config;
