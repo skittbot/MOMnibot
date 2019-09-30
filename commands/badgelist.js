f573667f6026ad01c4241b6df745881b63aa9a87
Jolene <sulfur.mermaid@gmail.com>
2019-09-30 19:03:21 -0400

    one more fix....?

--- commands/badgelist.js
+++ commands/badgelist.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   if (names.length < 1) return message.channel.send("There are currently no badges loaded into the database.");
   var eachBadge = [];
   for(var key of names) {
-    eachBadge.push({"name": key,"value":`[${client.badgeList.get(key,"description")}](${client.badgeList.get(key,"url")})`});
+    if(!(key === 'master list')) eachBadge.push({"name": key,"value":`[${client.badgeList.get(key,"description")}](${client.badgeList.get(key,"url")})`});
   }
   console.log(eachBadge);
   return message.channel.send('Here are a list of badges for this bot. To view what you have earned, use the following command: \n!profile',{
